078d99c59c3a2794a89742c431327a9ab1518400
arativathare51 <arati.r.vathare@gmail.com>
2026-04-24 23:47:37 +0530

    Added teammate screens + planner integration

---
+++ lib/screens/result_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ResultScreen extends StatelessWidget {
+  const ResultScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Result"),
+      ),
+      body: const Center(
+        child: Text("Result Predictor"),
+      ),
+    );
+  }
+}
